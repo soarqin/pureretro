f037713e6f54fddc94d1f8dd4ae46458695e4515
Soar Qin <soarchin@gmail.com>
2026-06-07 02:13:55 +0800

    Add per-backend video_backend vtable instances
    sw_backend / gl_backend / vk_backend are defined but not yet wired
    into video.c. Task 3 makes video.c dispatch through them.

--- src/video_sw.c
+++ src/video_sw.c
@@ -9,6 +9,7 @@
 #include <string.h>
 #include <SDL3/SDL.h>
 #include "video_sw.h"
+#include "video_backend.h"
 
 bool video_sw_init(SDL_Window *window, struct video_sw_context **out_ctx)
 {
@@ -117,3 +118,99 @@ void video_sw_present(struct video_sw_context *ctx, const void *data,
     SDL_RenderTexture(ctx->renderer, ctx->texture, NULL, &dst);
     SDL_RenderPresent(ctx->renderer);
 }
+
+/* ----- video_backend vtable adapters ----- */
+
+static bool vb_sw_match(enum retro_hw_context_type type)
+{
+    return type == RETRO_HW_CONTEXT_NONE;
+}
+
+static SDL_WindowFlags vb_sw_window_flags(void)
+{
+    return 0;
+}
+
+static bool vb_sw_init(SDL_Window *window, struct retro_hw_render_callback *hw,
+                       void **out_ctx)
+{
+    (void)hw;
+    struct video_sw_context *ctx = NULL;
+    if (!video_sw_init(window, &ctx))
+        return false;
+    *out_ctx = ctx;
+    return true;
+}
+
+static void vb_sw_destroy(void *ctx)
+{
+    video_sw_destroy((struct video_sw_context *)ctx);
+}
+
+static void vb_sw_present(void *ctx, const void *data, unsigned width,
+                          unsigned height, size_t pitch,
+                          enum retro_pixel_format fmt)
+{
+    video_sw_present((struct video_sw_context *)ctx, data, width, height,
+                     pitch, fmt);
+}
+
+static bool vb_sw_resize(void *ctx, SDL_Window *window,
+                         unsigned width, unsigned height)
+{
+    (void)ctx;
+    (void)window;
+    (void)width;
+    (void)height;
+    return true;
+}
+
+static uintptr_t vb_sw_get_current_framebuffer(void *ctx)
+{
+    (void)ctx;
+    return 0;
+}
+
+static retro_proc_address_t vb_sw_get_proc_address(void *ctx, const char *sym)
+{
+    (void)ctx;
+    (void)sym;
+    return NULL;
+}
+
+static bool vb_sw_negotiate_device(void *ctx,
+    const struct retro_hw_render_context_negotiation_interface *iface)
+{
+    (void)ctx;
+    (void)iface;
+    return false;
+}
+
+static bool vb_sw_get_hw_render_interface(void *ctx,
+    const struct retro_hw_render_interface **out_iface)
+{
+    (void)ctx;
+    (void)out_iface;
+    return false;
+}
+
+static void vb_sw_context_destroy(void *ctx)
+{
+    (void)ctx;
+}
+
+const struct video_backend sw_backend = {
+    .name                    = "sw",
+    .id                      = VIDEO_RENDERER_SW,
+    .match_hw_context        = vb_sw_match,
+    .window_flags            = vb_sw_window_flags,
+    .init                    = vb_sw_init,
+    .destroy                 = vb_sw_destroy,
+    .present                 = vb_sw_present,
+    .resize                  = vb_sw_resize,
+    .get_current_framebuffer = vb_sw_get_current_framebuffer,
+    .get_proc_address        = vb_sw_get_proc_address,
+    .negotiate_device        = vb_sw_negotiate_device,
+    .get_hw_render_interface = vb_sw_get_hw_render_interface,
+    .context_destroy         = vb_sw_context_destroy,
+};
